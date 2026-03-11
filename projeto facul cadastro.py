f39d34ae6655c3a2b2baef6ac5ec8e8bf9d020fa
KauaSennaStark <kauasennakrt@gmail.com>
2026-03-11 10:33:56 -0300

    formatação de cpf e edição de campo

--- projeto facul cadastro.py
+++ projeto facul cadastro.py
@@ -1,3 +1,14 @@
+#cliente teste
+def gerar_cliente_teste():
+    return {
+        "nome": "Usuário de Teste",
+        "cpf": "12345678900",
+        "data_nascimento": "01011990",
+        "cep": "12345678",
+        "endereco": "Rua de Teste, 123",
+        "email": "test@dominio.com",
+        "telefone": "000000000"
+    }
 clientes = []
 
 while True:
@@ -9,17 +20,24 @@ while True:
     if opcao == "1":  
         nome = input("nome: ")
         data_nascimento = input("Data de nascimento: ")
-        cpf = input("CPF: ")
+        data_nascimento_formatada = f"{data_nascimento[:2]}/{data_nascimento[2:4]}/{data_nascimento[4:]}"
+        cpf_numeros = input("CPF: ")
+        cpf_formatado = f"{cpf_numeros[:3]}.{cpf_numeros[3:6]}.{cpf_numeros[6:9]}-{cpf_numeros[9:]}"
         email = input("Email: ")
         telefone = input("Telefone: ")
+        telefone_formatado = f"({telefone[:2]}) {telefone[2:7]}-{telefone[7:]}"
         cep_numeros = input("Digite o Cep: ")
         cep_formatado = f"{cep_numeros[:5]}-{cep_numeros[5:]}"
         endereco = input("Endereço: ")
+        numero_residencia = input("Número da residência: ")
+        endereco_completo = f"{endereco}, {numero_residencia}"
+        informaçoes_adicionais = input("Informações adicionais: ")
+        
         cliente = {
             "nome": nome,
-            "cpf": cpf,
-            "data_nascimento": data_nascimento,
-            "telefone": telefone,
+            "cpf": cpf_formatado,
+            "data_nascimento": data_nascimento_formatada,
+            "telefone": telefone_formatado,
             "email": email,
             "cep": cep_formatado,
             "endereco": endereco
@@ -32,7 +50,7 @@ while True:
         busca = input("Digite o nome ou CPF do cliente para consulta: ")
         for a in clientes:
             if a['nome'].lower() == busca.lower() or a['cpf'] == busca:
-                print(f"localizado: {a['nome']}, CPF: {a['cpf']}, Tel: {a['telefone']}, Email: {a['email']}")
+                print(f"localizado: {a['nome']}, CPF: {a['cpf']}, Data de Nascimento: {a['data_nascimento']}, CEP: {a['cep']}, Endereço: {a['endereco']}, Telefone: {a['telefone']}, Email: {a['email']}")
                 break
         else:
             print("Cliente não encontrado!")
@@ -64,10 +82,9 @@ while True:
     elif opcao == "5":
         print("\n--- Lista de Clientes --")
         for i, a in enumerate(clientes):
-            print(f"{i} - Nome: {a['nome']}, CPF: {a['cpf']}, Tel: {a['telefone']}, Email: {a['email']}")
+            print(f"{i} - Nome: {a['nome']}, CPF: {a['cpf']}, Data de Nascimento: {a['data_nascimento']}, CEP: {a['cep']}, Tel: {a['telefone']}, Email: {a['email']}")
 
     
-#adicionar a formatação correta na digitação do cep e data de nascimento
 #adicionar data de nascimento e cep na consulta e atualização
 #configurar o menu para aceitar apenas números e tratar erros de digitação
 #adicionar a opção de listar clientes ordenados por nome ou CPF
